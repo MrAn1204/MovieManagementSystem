629ab2409eaaad6426222ebccb07725f40d1ae07
An Tran Dinh Khanh <trandinhkhanhan12042003@gmail.com>
2025-06-04 21:56:35 +0700

    Init project

---
+++ src/server/mms-api/src/main/java/com/mms/mms_api/MmsApiApplication.java
@@ -0,0 +1,13 @@
+package com.mms.mms_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MmsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MmsApiApplication.class, args);
+	}
+
+}
