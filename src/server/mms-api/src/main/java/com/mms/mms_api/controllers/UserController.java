376c936c758616550fb6b84449cf96c3ad579987
An Tran Dinh Khanh <trandinhkhanhan12042003@gmail.com>
2025-06-27 21:23:21 +0700

    Add UserController

---
+++ src/server/mms-api/src/main/java/com/mms/mms_api/controllers/UserController.java
@@ -0,0 +1,45 @@
+package com.mms.mms_api.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mms.mms_api.business.commands.UserCreateCommand;
+import com.mms.mms_api.business.services.UserService;
+import com.mms.mms_api.dto.UserDto;
+import com.mms.mms_api.models.User;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+    private UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public List<User> getAll() {
+        return List.of();
+    }
+    
+    @PostMapping("/create")
+    public ResponseEntity<UserDto> create(@RequestBody UserDto userDto) {
+        UserCreateCommand command = new UserCreateCommand(userDto);
+        
+        UserDto result = userService.handle(command);
+        
+        return result != null 
+            ? ResponseEntity.status(HttpStatus.CREATED).body(result) 
+            : ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+    }
+    
+}
