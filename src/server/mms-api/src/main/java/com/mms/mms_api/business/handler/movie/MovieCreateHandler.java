5d8fc7df6e26cf80265a2fa3757bbf36e2bea8ea
An Tran Dinh Khanh <trandinhkhanhan12042003@gmail.com>
2025-07-23 22:06:13 +0700

    Manually map genres, language, studios, and talents for new Movie

--- src/server/mms-api/src/main/java/com/mms/mms_api/business/handler/movie/MovieCreateHandler.java
+++ src/server/mms-api/src/main/java/com/mms/mms_api/business/handler/movie/MovieCreateHandler.java
@@ -1,26 +1,64 @@
 package com.mms.mms_api.business.handler.movie;
 
+import java.util.List;
+
 import com.mms.mms_api.business.command.movie.MovieCreateCommand;
+import com.mms.mms_api.data.GenreRepository;
+import com.mms.mms_api.data.LanguageRepository;
 import com.mms.mms_api.data.MovieRepository;
+import com.mms.mms_api.data.StudioRepository;
+import com.mms.mms_api.data.TalentRepository;
 import com.mms.mms_api.dto.MovieDto;
+import com.mms.mms_api.model.Genre;
+import com.mms.mms_api.model.Language;
 import com.mms.mms_api.model.Movie;
+import com.mms.mms_api.model.Studio;
+import com.mms.mms_api.model.Talent;
 import com.mms.mms_api.util.mapper.MovieMapper;
 
 public class MovieCreateHandler extends MovieBaseHandler<MovieCreateCommand, MovieDto> {
+    private GenreRepository genreRepository;
+
+    private LanguageRepository languageRepository;
+
+    private StudioRepository studioRepository;
+
+    private TalentRepository talentRepository;
+
     public MovieCreateHandler(
-        MovieCreateCommand request,
-        MovieMapper movieMapper,
-        MovieRepository movieRepository
-    ) {
+            MovieCreateCommand request,
+            MovieMapper movieMapper,
+            MovieRepository movieRepository,
+            GenreRepository genreRepository,
+            LanguageRepository languageRepository,
+            StudioRepository studioRepository,
+            TalentRepository talentRepository) {
         super(request, movieMapper, movieRepository);
         this.movieRepository = movieRepository;
+        this.genreRepository = genreRepository;
+        this.languageRepository = languageRepository;
+        this.studioRepository = studioRepository;
+        this.talentRepository = talentRepository;
     }
 
     public MovieDto execute() {
         Movie movie = movieMapper.toEntity(request);
 
-        Movie savedMovie = movieRepository.save(movie);
+        List<Genre> genres = genreRepository.findByNameIn(request.getGenres());
 
+        Language language = languageRepository.findByName(request.getLanguage());
+
+        List<Studio> studios = studioRepository.findByNameIn(request.getStudios());
+
+        List<Talent> talents = talentRepository.findByNameIn(request.getTalents());
+
+        movie.setGenres(genres);
+        movie.setLanguage(language);
+        movie.setStudios(studios);
+        movie.setTalents(talents);
+
+        Movie savedMovie = movieRepository.save(movie);
+        
         return movieMapper.toDto(savedMovie);
     }
 }
--- src/server/mms-api/src/main/java/com/mms/mms_api/business/service/MovieService.java
+++ src/server/mms-api/src/main/java/com/mms/mms_api/business/service/MovieService.java
@@ -8,7 +8,11 @@ import com.mms.mms_api.business.handler.movie.MovieGetAllHandler;
 import com.mms.mms_api.business.handler.movie.MovieGetByIdHandler;
 import com.mms.mms_api.business.query.movie.MovieGetAllQuery;
 import com.mms.mms_api.business.query.movie.MovieGetByIdQuery;
+import com.mms.mms_api.data.GenreRepository;
+import com.mms.mms_api.data.LanguageRepository;
 import com.mms.mms_api.data.MovieRepository;
+import com.mms.mms_api.data.StudioRepository;
+import com.mms.mms_api.data.TalentRepository;
 import com.mms.mms_api.dto.MovieDto;
 import com.mms.mms_api.util.mapper.MovieMapper;
 
@@ -20,11 +24,20 @@ import org.springframework.stereotype.Service;
 @AllArgsConstructor
 public class MovieService {
     private final MovieRepository movieRepository;
+
+    private final GenreRepository genreRepository;
+
+    private final LanguageRepository languageRepository;
+
+    private final StudioRepository studioRepository;
+
+    private final TalentRepository talentRepository;
     
     private final MovieMapper movieMapper;
 
     public MovieDto handle(MovieCreateCommand request) {
-        MovieCreateHandler handler = new MovieCreateHandler(request, movieMapper, movieRepository);
+        MovieCreateHandler handler = new MovieCreateHandler(request, movieMapper, movieRepository,
+                genreRepository, languageRepository, studioRepository, talentRepository);
         return handler.execute();
     }
 
