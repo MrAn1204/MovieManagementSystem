bb06255b23c4a589dc8497965838285e28191c5d
An Tran Dinh Khanh <trandinhkhanhan12042003@gmail.com>
2025-08-07 21:52:59 +0700

    Improve movie search function with studio, talent, genre, and language criteria

--- src/server/mms-api/src/main/java/com/mms/mms_api/business/query/movie/MovieSearchQuery.java
+++ src/server/mms-api/src/main/java/com/mms/mms_api/business/query/movie/MovieSearchQuery.java
@@ -1,5 +1,7 @@
 package com.mms.mms_api.business.query.movie;
 
+import java.util.List;
+
 import com.mms.mms_api.business.query.BaseSearchQuery;
 import com.mms.mms_api.business.query.SortDirection;
 
@@ -9,9 +11,15 @@ import lombok.EqualsAndHashCode;
 @Data
 @EqualsAndHashCode(callSuper = true)
 public class MovieSearchQuery extends BaseSearchQuery {
+    private List<String> genres;
+    
+    private String language;
 
-    public MovieSearchQuery(int pageNumber, int pageSize, String keyword, String sortBy, SortDirection sortDirection) {
+    public MovieSearchQuery(int pageNumber, int pageSize, String keyword, String sortBy, SortDirection sortDirection,
+            List<String> genres, String language) {
         super(pageNumber, pageSize, keyword, sortBy, sortDirection);
+        this.language = language;
+        this.genres = genres;
     }
 
 }
--- src/server/mms-api/src/main/java/com/mms/mms_api/business/specification/MovieSpecification.java
+++ src/server/mms-api/src/main/java/com/mms/mms_api/business/specification/MovieSpecification.java
@@ -6,12 +6,18 @@ import java.util.List;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.lang.NonNull;
 import org.springframework.lang.Nullable;
+import org.springframework.util.CollectionUtils;
 import org.springframework.util.StringUtils;
 
 import com.mms.mms_api.business.query.movie.MovieSearchQuery;
+import com.mms.mms_api.model.Language;
 import com.mms.mms_api.model.Movie;
+import com.mms.mms_api.model.Studio;
+import com.mms.mms_api.model.Talent;
+
 import jakarta.persistence.criteria.CriteriaBuilder;
 import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Join;
 import jakarta.persistence.criteria.Predicate;
 import jakarta.persistence.criteria.Root;
 
@@ -33,12 +39,46 @@ public class MovieSpecification implements Specification<Movie> {
             predicates.add(keywordPredicate);
         }
 
+        if (CollectionUtils.isEmpty(criteria.getGenres())) {
+            Predicate genrePredicate = addGenrePredicate(root);
+
+            predicates.add(genrePredicate);
+        }
+
+        if (StringUtils.hasText(criteria.getLanguage())) {
+            Predicate languagePredicate = addLanguagePredicate(root, criteriaBuilder);
+
+            predicates.add(languagePredicate);
+        }
+
         return criteriaBuilder.and(predicates.toArray(Predicate[]::new));
     }
 
     private Predicate addKeywordPredicate(Root<Movie> root, CriteriaBuilder criteriaBuilder) {
         String pattern = "%" + criteria.getKeyword().toLowerCase() + "%";
 
-        return criteriaBuilder.like(root.get("name"), pattern);
+        Join<Movie, Studio> studioJoin = root.join("studios");
+        Join<Movie, Talent> talentJoin = root.join("talents");
+
+        Predicate namePredicate = criteriaBuilder.like(root.get("name"), pattern);
+        Predicate studioPredicate = criteriaBuilder.like(studioJoin.get("name"), pattern);
+        Predicate talentPredicate = criteriaBuilder.like(talentJoin.get("name"), pattern);
+
+        return criteriaBuilder.or(namePredicate, studioPredicate, talentPredicate);
+    }
+
+    private Predicate addGenrePredicate(Root<Movie> root) {
+        List<String> searchGenres = criteria.getGenres().stream()
+                .map(genre -> StringUtils.capitalize(genre.toLowerCase())).toList();
+
+        return root.get("language").in(searchGenres);
+    }
+
+    private Predicate addLanguagePredicate(Root<Movie> root, CriteriaBuilder criteriaBuilder) {
+        String pattern = criteria.getLanguage().toUpperCase();
+
+        Join<Movie, Language> languageJoin = root.join("language");
+
+        return criteriaBuilder.equal(languageJoin.get("name"), pattern);
     }
 }
