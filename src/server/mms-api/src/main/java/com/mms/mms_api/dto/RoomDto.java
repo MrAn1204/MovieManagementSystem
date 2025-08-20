72ebd8f4cd94031849eaf83976c1e63bd34fccf5
An Tran Dinh Khanh <trandinhkhanhan12042003@gmail.com>
2025-08-20 21:02:22 +0700

    Add id field to RoomDto and update RoomMapper configuration to ignore null mapping

--- src/server/mms-api/src/main/java/com/mms/mms_api/dto/RoomDto.java
+++ src/server/mms-api/src/main/java/com/mms/mms_api/dto/RoomDto.java
@@ -1,11 +1,14 @@
 package com.mms.mms_api.dto;
 
 import java.util.List;
+import java.util.UUID;
 
 import lombok.Data;
 
 @Data
 public class RoomDto {
+    private UUID id;
+
     private int seatQuantity;
 
     private String name;
--- src/server/mms-api/src/main/java/com/mms/mms_api/util/mapper/RoomMapper.java
+++ src/server/mms-api/src/main/java/com/mms/mms_api/util/mapper/RoomMapper.java
@@ -3,13 +3,14 @@ package com.mms.mms_api.util.mapper;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
 
 import com.mms.mms_api.business.command.room.RoomCreateCommand;
 import com.mms.mms_api.business.command.room.RoomUpdateCommand;
 import com.mms.mms_api.dto.RoomDto;
 import com.mms.mms_api.model.Room;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
 public interface RoomMapper {
     @Mapping(target = "id", ignore = true)
     @Mapping(target = "seats", ignore = true)
@@ -18,7 +19,6 @@ public interface RoomMapper {
     @Mapping(target = "seats", ignore = true)
     RoomDto toDto(Room room);
 
-    @Mapping(target = "id", ignore = true)
     @Mapping(target = "seats", ignore = true)
     void updateEntity(RoomUpdateCommand command, @MappingTarget Room room);
 }
