a9406f6d86d7343a403a3a1fac62f73295b7a470
An Tran Dinh Khanh <trandinhkhanhan12042003@gmail.com>
2025-08-22 21:29:25 +0700

    Add AppConstant class that defines application constants

---
+++ src/server/mms-api/src/main/java/com/mms/mms_api/common/AppConstant.java
@@ -0,0 +1,9 @@
+package com.mms.mms_api.common;
+
+public class AppConstant {
+    private AppConstant() {}
+
+    public static final int ROW_MAX = 10;
+
+    public static final int COLUMN_MAX = 10;
+}
