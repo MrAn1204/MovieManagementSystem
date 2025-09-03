ba45c5a4af7362ab158ade28bf1d5c454992215d
An Tran Dinh Khanh <trandinhkhanhan12042003@gmail.com>
2025-09-03 10:59:31 +0700

    Implement SeatService and SeatController

---
+++ src/server/mms-api/src/main/java/com/mms/mms_api/business/service/SeatService.java
@@ -0,0 +1,57 @@
+package com.mms.mms_api.business.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.mms.mms_api.business.command.seat.SeatCreateCommand;
+import com.mms.mms_api.business.command.seat.SeatDeleteCommand;
+import com.mms.mms_api.business.command.seat.SeatUpdateCommand;
+import com.mms.mms_api.business.handler.seat.SeatCreateHandler;
+import com.mms.mms_api.business.handler.seat.SeatDeleteHandler;
+import com.mms.mms_api.business.handler.seat.SeatGetAllHandler;
+import com.mms.mms_api.business.handler.seat.SeatGetByIdHandler;
+import com.mms.mms_api.business.handler.seat.SeatUpdateHandler;
+import com.mms.mms_api.business.query.seat.SeatGetAllQuery;
+import com.mms.mms_api.business.query.seat.SeatGetByIdQuery;
+import com.mms.mms_api.data.RoomRepository;
+import com.mms.mms_api.data.SeatRepository;
+import com.mms.mms_api.dto.SeatDto;
+import com.mms.mms_api.util.mapper.SeatMapper;
+
+import lombok.AllArgsConstructor;
+
+@Service
+@AllArgsConstructor
+public class SeatService {
+    private final SeatRepository seatRepository;
+
+    private final RoomRepository roomRepository;
+
+    private final SeatMapper seatMapper;
+
+    public SeatDto handle(SeatCreateCommand request) {
+        SeatCreateHandler handler = new SeatCreateHandler(request, seatMapper, seatRepository, roomRepository);
+        return handler.execute();
+    }
+
+    public List<SeatDto> handle(SeatGetAllQuery request) {
+        SeatGetAllHandler handler = new SeatGetAllHandler(request, seatMapper, seatRepository); 
+        return handler.execute();
+    }
+
+    public SeatDto handle(SeatGetByIdQuery request) {
+        SeatGetByIdHandler handler = new SeatGetByIdHandler(request, seatMapper, seatRepository);
+        return handler.execute();
+    }
+
+    public SeatDto handle(SeatUpdateCommand request) {
+        SeatUpdateHandler handler = new SeatUpdateHandler(request, seatMapper, seatRepository, roomRepository);
+        return handler.execute();
+    }
+
+    public void handle(SeatDeleteCommand request) {
+        SeatDeleteHandler handler = new SeatDeleteHandler(request, seatMapper, seatRepository);
+        handler.execute();
+    }
+}
---
+++ src/server/mms-api/src/main/java/com/mms/mms_api/controller/SeatController.java
@@ -0,0 +1,65 @@
+package com.mms.mms_api.controller;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mms.mms_api.business.command.seat.SeatCreateCommand;
+import com.mms.mms_api.business.command.seat.SeatDeleteCommand;
+import com.mms.mms_api.business.command.seat.SeatUpdateCommand;
+import com.mms.mms_api.business.query.seat.SeatGetAllQuery;
+import com.mms.mms_api.business.query.seat.SeatGetByIdQuery;
+import com.mms.mms_api.business.service.SeatService;
+import com.mms.mms_api.dto.SeatDto;
+
+import lombok.AllArgsConstructor;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+
+@RestController
+@RequestMapping("/api/seats")
+@AllArgsConstructor
+public class SeatController {
+    private final SeatService seatService;
+
+    @PostMapping("/create")
+    public ResponseEntity<SeatDto> create(@RequestBody SeatCreateCommand request) {
+        return ResponseEntity.ok(seatService.handle(request));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<SeatDto>> getAll() {
+        List<SeatDto> seats = seatService.handle(new SeatGetAllQuery());
+        return ResponseEntity.ok(seats);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<SeatDto> getById(@PathVariable UUID id) {
+        SeatDto seat = seatService.handle(new SeatGetByIdQuery(id));
+        return ResponseEntity.ok(seat);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<SeatDto> update(@PathVariable UUID id, @RequestBody SeatUpdateCommand request) {
+        request.setId(id);
+        SeatDto updatedSeat = seatService.handle(request);
+        if (updatedSeat == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(updatedSeat);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable UUID id) {
+        seatService.handle(new SeatDeleteCommand(id));
+        return ResponseEntity.noContent().build();
+    }
+}
