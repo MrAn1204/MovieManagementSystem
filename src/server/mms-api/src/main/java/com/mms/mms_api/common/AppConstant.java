273ff5131e709532b4c0c9e79c5633094fe51f1a
An Tran Dinh Khanh <trandinhkhanhan12042003@gmail.com>
2025-09-22 10:40:20 +0700

    Add additional constants for user input validation

--- src/server/mms-api/src/main/java/com/mms/mms_api/common/AppConstant.java
+++ src/server/mms-api/src/main/java/com/mms/mms_api/common/AppConstant.java
@@ -3,6 +3,26 @@ package com.mms.mms_api.common;
 public class AppConstant {
     private AppConstant() {}
 
+    public static final int USERNAME_MIN = 5;
+
+    public static final int USERNAME_MAX = 20;
+
+    public static final int FULLNAME_MIN = 3;
+
+    public static final int FULLNAME_MAX = 50;
+
+    public static final int PASSWORD_MIN = 8;
+
+    public static final int CITIZEN_ID_MIN = 12;
+
+    public static final int PHONE_MIN = 10;
+
+    public static final int PHONE_MAX = 15;
+
+    public static final int ADDRESS_MIN = 5;
+
+    public static final int ADDRESS_MAX = 100;
+
     public static final int ROW_MAX = 10;
 
     public static final int COLUMN_MAX = 10;
