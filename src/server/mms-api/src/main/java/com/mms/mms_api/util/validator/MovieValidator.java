ac8f28b9638a92aac7a7a83f4eb8c32238be589c
An Tran Dinh Khanh <trandinhkhanhan12042003@gmail.com>
2025-09-23 16:59:36 +0700

    Remove unused methods and update error messages in MovieValidator

--- src/server/mms-api/src/main/java/com/mms/mms_api/util/validator/MovieValidator.java
+++ src/server/mms-api/src/main/java/com/mms/mms_api/util/validator/MovieValidator.java
@@ -1,6 +1,5 @@
 package com.mms.mms_api.util.validator;
 
-import com.mms.mms_api.exception.ErrorMessage;
 import com.mms.mms_api.exception.InvalidInputException;
 import com.mms.mms_api.model.Genre;
 import com.mms.mms_api.model.Language;
@@ -14,33 +13,27 @@ import java.util.List;
 public class MovieValidator {
     private MovieValidator() {}
 
-    public static void validateName(String name) {
-        if (!StringUtils.hasText(name)) {
-            throw new InvalidInputException(ErrorMessage.NAME_REQUIRED);
-        }
-    }
-
     public static void validateGenres(List<String> genreNames, List<Genre> mappedNames) {
         if (!CollectionUtils.isEmpty(genreNames) && genreNames.size() != mappedNames.size()) {
-            throw new InvalidInputException(ErrorMessage.GENRES_INVALID);
+            throw new InvalidInputException("movie.genres.invalid");
         }
     }
 
     public static void validateLanguage(String languageName, Language mappedLanguage) {
         if (StringUtils.hasText(languageName) && mappedLanguage != null) {
-            throw new InvalidInputException(ErrorMessage.LANGUAGE_INVALID);
+            throw new InvalidInputException("movie.language.invalid");
         }
     }
 
     public static void validateStudios(List<String> studioNames, List<Studio> mappedStudios) {
         if (!CollectionUtils.isEmpty(studioNames) && studioNames.size() != mappedStudios.size()) {
-            throw new InvalidInputException(ErrorMessage.STUDIOS_INVALID);
+            throw new InvalidInputException("movie.studios.invalid");
         }
     }
 
     public static void validateTalents(List<String> talentNames, List<Talent> mappedTalents) {
         if (!CollectionUtils.isEmpty(talentNames) && talentNames.size() != mappedTalents.size()) {
-            throw new InvalidInputException(ErrorMessage.TALENTS_INVALID);
+            throw new InvalidInputException("movie.talents.invalid");
         }
     }
 }
