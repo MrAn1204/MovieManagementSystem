2192a6e8093b3ccf9ed918aedd23fafd9587abd3
An Tran Dinh Khanh <trandinhkhanhan12042003@gmail.com>
2025-09-23 10:02:25 +0700

    Add validation annotations to UserCreateCommand and UserUpdateCommand

--- src/server/mms-api/src/main/java/com/mms/mms_api/business/command/user/UserUpdateCommand.java
+++ src/server/mms-api/src/main/java/com/mms/mms_api/business/command/user/UserUpdateCommand.java
@@ -4,7 +4,15 @@ import java.time.LocalDate;
 import java.util.List;
 
 import com.mms.mms_api.business.command.BaseUpdateCommand;
+import com.mms.mms_api.common.AppConstant;
 import com.mms.mms_api.model.Gender;
+import com.mms.mms_api.validation.Password;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Size;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -14,25 +22,39 @@ import lombok.EqualsAndHashCode;
 @EqualsAndHashCode(callSuper = true)
 @AllArgsConstructor
 public class UserUpdateCommand extends BaseUpdateCommand {
+    @NotNull(message = "{user.username.required}")
+    @Size(min = AppConstant.USERNAME_MIN, max = AppConstant.USERNAME_MAX, message = "{user.username.size}")
     private String username;
 
+    @NotNull(message = "{user.fullname.required}")
+    @Size(min = AppConstant.FULLNAME_MIN, max = AppConstant.FULLNAME_MAX, message = "{user.fullname.size}")
     private String fullname;
 
+    @Password
     private String password;
 
+    @NotNull(message = "{user.gender.required}")
     private Gender gender;
 
+    @NotNull(message = "{user.dob.required}")
+    @Past(message = "{user.dob.past}")
     private LocalDate dateOfBirth;
 
+    @Email(message = "{user.email.invalid}")
     private String email;
 
+    @Size(min = AppConstant.CITIZEN_ID_MIN, message = "{user.citizenId.size}")
     private String citizenIdNumber;
 
+    @NotNull(message = "{user.phone.required}")
+    @Size(min = AppConstant.PHONE_MIN, max = AppConstant.PHONE_MAX, message = "{user.phone.size}")
     private String phoneNumber;
 
+    @Size(min = AppConstant.ADDRESS_MIN, max = AppConstant.ADDRESS_MAX, message = "{user.address.size}")
     private String address;
 
     private int score;
 
+    @NotEmpty(message = "{user.roles.required}")
     private List<String> roles;
 }
