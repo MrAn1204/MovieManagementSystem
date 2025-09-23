17135f624ee698997dfda59bf788c5a1b8be5af1
An Tran Dinh Khanh <trandinhkhanhan12042003@gmail.com>
2025-09-23 16:58:57 +0700

    Remove unused methods and update error messages for role validation in UserValidator

--- src/server/mms-api/src/main/java/com/mms/mms_api/util/validator/UserValidator.java
+++ src/server/mms-api/src/main/java/com/mms/mms_api/util/validator/UserValidator.java
@@ -1,63 +1,22 @@
 package com.mms.mms_api.util.validator;
 
-import java.time.LocalDate;
 import java.util.List;
 
-import org.springframework.util.StringUtils;
 import org.springframework.util.CollectionUtils;
 
-import com.mms.mms_api.exception.ErrorMessage;
 import com.mms.mms_api.exception.InvalidInputException;
 import com.mms.mms_api.model.Role;
-import com.mms.mms_api.util.PasswordValidator;
 
 public class UserValidator {
     private UserValidator() {} 
 
-    public static void validateUsername(String username) {
-        if (!StringUtils.hasText(username)) {
-            throw new InvalidInputException(ErrorMessage.USERNAME_REQUIRED);
-        }
-    }
-
-    public static void validateFullname(String fullname) {
-        if (!StringUtils.hasText(fullname)) {
-            throw new InvalidInputException(ErrorMessage.FULLNAME_REQUIRED);
-        }
-    }
-
-    public static void validatePassword(String password) {
-        List<ErrorMessage> passwordErrors = PasswordValidator.validate(password);
-        if (!passwordErrors.isEmpty()) {
-            throw new InvalidInputException(passwordErrors.get(0));
-        }
-    }
-
-    public static void validateGender(Object gender) {
-        if (gender == null) {
-            throw new InvalidInputException(ErrorMessage.GENDER_REQUIRED);
-        }
-    }
-
-    public static void validateDateOfBirth(LocalDate dateOfBirth) {
-        if (dateOfBirth == null) {
-            throw new InvalidInputException(ErrorMessage.DOB_REQUIRED);
-        }
-    }
-
-    public static void validatePhoneNumber(String phoneNumber) {
-        if (!StringUtils.hasText(phoneNumber)) {
-            throw new InvalidInputException(ErrorMessage.PHONE_REQUIRED);
-        }
-    }
-
     public static void validateRoles(List<String> rolesNames, List<Role> mappedRoles) {
         if (CollectionUtils.isEmpty(rolesNames)) {
-            throw new InvalidInputException(ErrorMessage.ROLES_REQUIRED);
+            throw new InvalidInputException("user.roles.required");
         }
 
         if (mappedRoles.size() != rolesNames.size()) {
-            throw new InvalidInputException(ErrorMessage.ROLES_INVALID);
+            throw new InvalidInputException("user.roles.invalid");
         }
     }
 }
