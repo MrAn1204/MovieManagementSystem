65b0f4e7b9f218c4941343bfa2ca2fcef215d417
An Tran Dinh Khanh <trandinhkhanhan12042003@gmail.com>
2025-09-26 21:00:04 +0700

    Add @NonNull annotations to ScheduleSeat fields

--- src/server/mms-api/src/main/java/com/mms/mms_api/model/ScheduleSeat.java
+++ src/server/mms-api/src/main/java/com/mms/mms_api/model/ScheduleSeat.java
@@ -1,5 +1,7 @@
 package com.mms.mms_api.model;
 
+import org.springframework.lang.NonNull;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
@@ -9,23 +11,28 @@ import jakarta.persistence.MapsId;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 
 @Entity
 @Data
 @NoArgsConstructor
+@RequiredArgsConstructor
 @AllArgsConstructor
 public class ScheduleSeat {
     @EmbeddedId
+    @NonNull
     private ScheduleSeatId id;
 
     @Column(nullable = false)
     private boolean reserved = false;
 
+    @NonNull
     @ManyToOne
     @MapsId("scheduleId")
     @JoinColumn(name = "schedule_id")
     private Schedule schedule;
 
+    @NonNull
     @ManyToOne
     @MapsId("seatId")
     @JoinColumn(name = "seat_id")
