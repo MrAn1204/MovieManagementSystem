3af2faa44f2cc79d5183cc39c441d2d54c67fee4
An Tran Dinh Khanh <trandinhkhanhan12042003@gmail.com>
2025-09-30 15:21:40 +0700

    Add BaseTicketHandler abstract class

---
+++ src/server/mms-api/src/main/java/com/mms/mms_api/business/handler/ticket/BaseTicketHandler.java
@@ -0,0 +1,18 @@
+package com.mms.mms_api.business.handler.ticket;
+
+import com.mms.mms_api.business.handler.BaseHandler;
+import com.mms.mms_api.data.TicketRepository;
+import com.mms.mms_api.util.mapper.TicketMapper;
+
+public abstract class BaseTicketHandler<I, O> extends BaseHandler<I, O> {
+    protected TicketMapper ticketMapper;
+    
+    protected TicketRepository ticketRepository;
+    
+    protected BaseTicketHandler(I request, TicketMapper ticketMapper, TicketRepository ticketRepository) {
+        super(request);
+        this.ticketMapper = ticketMapper;
+        this.ticketRepository = ticketRepository;
+    }
+
+}
