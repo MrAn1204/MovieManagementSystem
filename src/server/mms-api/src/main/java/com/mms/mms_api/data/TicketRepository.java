f8225f3bf5a40e8c2a1745b259e78e687d5b622f
An Tran Dinh Khanh <trandinhkhanhan12042003@gmail.com>
2025-09-30 15:20:28 +0700

    Add TicketRepository and TicketDto

---
+++ src/server/mms-api/src/main/java/com/mms/mms_api/data/TicketRepository.java
@@ -0,0 +1,11 @@
+package com.mms.mms_api.data;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.mms.mms_api.model.Ticket;
+
+public interface TicketRepository extends JpaRepository<Ticket, UUID> {
+
+}
---
+++ src/server/mms-api/src/main/java/com/mms/mms_api/dto/TicketDto.java
@@ -0,0 +1,14 @@
+package com.mms.mms_api.dto;
+
+import java.util.UUID;
+
+import lombok.Data;
+
+@Data
+public class TicketDto {
+    UUID id;
+
+    ScheduleDto schedule;
+
+    SeatDto seat; 
+}
