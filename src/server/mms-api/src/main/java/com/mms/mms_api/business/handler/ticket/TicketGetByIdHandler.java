8163311e261c9149e9a5104b95e54b945b26fe31
An Tran Dinh Khanh <trandinhkhanhan12042003@gmail.com>
2025-10-01 10:14:13 +0700

    Add TicketGetAllHandler and TicketGetByIdHandler for ticket retrieval functionality

---
+++ src/server/mms-api/src/main/java/com/mms/mms_api/business/handler/ticket/TicketGetByIdHandler.java
@@ -0,0 +1,22 @@
+package com.mms.mms_api.business.handler.ticket;
+
+import com.mms.mms_api.business.query.ticket.TicketGetByIdQuery;
+import com.mms.mms_api.dto.TicketDto;
+import com.mms.mms_api.exception.ResourceNotFoundException;
+import com.mms.mms_api.data.TicketRepository;
+import com.mms.mms_api.util.mapper.TicketMapper;
+import com.mms.mms_api.model.Ticket;
+
+public class TicketGetByIdHandler extends BaseTicketHandler<TicketGetByIdQuery, TicketDto> {
+
+    public TicketGetByIdHandler(TicketGetByIdQuery request, TicketMapper ticketMapper, TicketRepository ticketRepository) {
+        super(request, ticketMapper, ticketRepository);
+    }
+
+    @Override
+    public TicketDto execute() {
+        Ticket ticket = ticketRepository.findById(request.getId())
+                .orElseThrow(() -> new ResourceNotFoundException("ticket.notFound"));
+        return ticketMapper.toDto(ticket);
+    }
+}
