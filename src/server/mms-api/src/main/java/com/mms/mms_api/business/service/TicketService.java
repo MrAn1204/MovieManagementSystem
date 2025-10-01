a512fc5271619b57cf4601788125b87ffa6a5f06
An Tran Dinh Khanh <trandinhkhanhan12042003@gmail.com>
2025-10-01 10:19:13 +0700

    Add ticket retrieval endpoints in TicketController and implement handling in TicketService

--- src/server/mms-api/src/main/java/com/mms/mms_api/business/service/TicketService.java
+++ src/server/mms-api/src/main/java/com/mms/mms_api/business/service/TicketService.java
@@ -1,9 +1,15 @@
 package com.mms.mms_api.business.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 import com.mms.mms_api.business.command.ticket.TicketCreateCommand;
 import com.mms.mms_api.business.handler.ticket.TicketCreateHandler;
+import com.mms.mms_api.business.handler.ticket.TicketGetAllHandler;
+import com.mms.mms_api.business.handler.ticket.TicketGetByIdHandler;
+import com.mms.mms_api.business.query.ticket.TicketGetAllQuery;
+import com.mms.mms_api.business.query.ticket.TicketGetByIdQuery;
 import com.mms.mms_api.data.ScheduleRepository;
 import com.mms.mms_api.data.SeatRepository;
 import com.mms.mms_api.data.TicketRepository;
@@ -29,6 +35,16 @@ public class TicketService {
 
     public TicketDto handle(TicketCreateCommand request) {
         TicketCreateHandler handler = new TicketCreateHandler(request, ticketMapper, ticketRepository, scheduleRepository, seatRepository);
+        return handler.execute();
+    }
+
+    public List<TicketDto> handle(TicketGetAllQuery request) {
+        TicketGetAllHandler handler = new TicketGetAllHandler(request, ticketMapper, ticketRepository);
+        return handler.execute();
+    }
+
+    public TicketDto handle(TicketGetByIdQuery request) {
+        TicketGetByIdHandler handler = new TicketGetByIdHandler(request, ticketMapper, ticketRepository);
         return handler.execute();
     }
 }
--- src/server/mms-api/src/main/java/com/mms/mms_api/controller/TicketController.java
+++ src/server/mms-api/src/main/java/com/mms/mms_api/controller/TicketController.java
@@ -1,5 +1,8 @@
 package com.mms.mms_api.controller;
 
+import java.util.List;
+import java.util.UUID;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -8,10 +11,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.mms.mms_api.business.command.ticket.TicketCreateCommand;
+import com.mms.mms_api.business.query.ticket.TicketGetAllQuery;
+import com.mms.mms_api.business.query.ticket.TicketGetByIdQuery;
 import com.mms.mms_api.business.service.TicketService;
 import com.mms.mms_api.dto.TicketDto;
 
 import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
 @RequestMapping("/api/tickets")
@@ -24,5 +31,19 @@ public class TicketController {
         TicketDto ticket = ticketService.handle(command);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(ticket);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<TicketDto>> getAll() {
+        List<TicketDto> tickets = ticketService.handle(new TicketGetAllQuery());
+
+        return ResponseEntity.ok(tickets);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<TicketDto> getById(@PathVariable UUID id) {
+        TicketDto ticket = ticketService.handle(new TicketGetByIdQuery(id));
+
+        return ResponseEntity.ok(ticket);
     }
 }
