7985302572562309f755d5209b7eb9254de3b884
An Tran Dinh Khanh <trandinhkhanhan12042003@gmail.com>
2025-10-01 10:39:37 +0700

    Add TicketDeleteHandler for processing ticket deletion requests

---
+++ src/server/mms-api/src/main/java/com/mms/mms_api/business/handler/ticket/TicketDeleteHandler.java
@@ -0,0 +1,22 @@
+package com.mms.mms_api.business.handler.ticket;
+
+import com.mms.mms_api.business.command.ticket.TicketDeleteCommand;
+import com.mms.mms_api.data.TicketRepository;
+import com.mms.mms_api.exception.ResourceNotFoundException;
+import com.mms.mms_api.model.Ticket;
+
+public class TicketDeleteHandler extends BaseTicketHandler<TicketDeleteCommand, Void> {
+
+    public TicketDeleteHandler(TicketDeleteCommand request, TicketRepository ticketRepository) {
+        super(request, null, ticketRepository);
+    }
+
+    @Override
+    public Void execute() {
+        Ticket ticket = ticketRepository.findById(request.getId())
+                .orElseThrow(() -> new ResourceNotFoundException("ticket.notFound"));
+
+        ticketRepository.delete(ticket);
+        return null;
+    }
+}
