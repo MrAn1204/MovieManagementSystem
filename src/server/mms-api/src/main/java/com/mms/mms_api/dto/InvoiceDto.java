b83b65e2f650cbc1c755243c79fb8d62117971d4
An Tran Dinh Khanh <trandinhkhanhan12042003@gmail.com>
2025-10-02 11:46:08 +0700

    Add id field to InvoiceDto

--- src/server/mms-api/src/main/java/com/mms/mms_api/dto/InvoiceDto.java
+++ src/server/mms-api/src/main/java/com/mms/mms_api/dto/InvoiceDto.java
@@ -1,11 +1,14 @@
 package com.mms.mms_api.dto;
 
 import java.util.List;
+import java.util.UUID;
 
 import lombok.Data;
 
 @Data
 public class InvoiceDto {
+    private UUID id;
+
     private int totalMoney;
 
     private int addScore;
