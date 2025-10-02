7d12bcc6bc2ca3c053cc4a98807ac20ab516ce2c
An Tran Dinh Khanh <trandinhkhanhan12042003@gmail.com>
2025-10-02 10:46:25 +0700

    Add method to find tickets by a list of IDs in TicketRepository

--- src/server/mms-api/src/main/java/com/mms/mms_api/data/TicketRepository.java
+++ src/server/mms-api/src/main/java/com/mms/mms_api/data/TicketRepository.java
@@ -1,5 +1,6 @@
 package com.mms.mms_api.data;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -7,5 +8,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.mms.mms_api.model.Ticket;
 
 public interface TicketRepository extends JpaRepository<Ticket, UUID> {
-
+    List<Ticket> findByIdIn(List<UUID> ids);
 }
