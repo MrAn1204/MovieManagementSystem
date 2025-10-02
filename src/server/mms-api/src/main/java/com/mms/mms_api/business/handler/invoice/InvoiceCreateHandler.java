f3e5f6f8cbcff754b419d597240a35ddc03b80e8
An Tran Dinh Khanh <trandinhkhanhan12042003@gmail.com>
2025-10-02 10:47:24 +0700

    Add InvoiceCreateHandler to handle invoice creation

---
+++ src/server/mms-api/src/main/java/com/mms/mms_api/business/handler/invoice/InvoiceCreateHandler.java
@@ -0,0 +1,39 @@
+package com.mms.mms_api.business.handler.invoice;
+
+import com.mms.mms_api.business.command.invoice.InvoiceCreateCommand;
+import com.mms.mms_api.dto.InvoiceDto;
+import com.mms.mms_api.exception.InvalidInputException;
+import com.mms.mms_api.data.InvoiceRepository;
+import com.mms.mms_api.data.TicketRepository;
+import com.mms.mms_api.model.Invoice;
+import com.mms.mms_api.model.Ticket;
+import com.mms.mms_api.util.mapper.InvoiceMapper;
+
+import java.util.List;
+
+public class InvoiceCreateHandler extends InvoiceBaseHandler<InvoiceCreateCommand, InvoiceDto> {
+
+    private final TicketRepository ticketRepository;
+
+    public InvoiceCreateHandler(InvoiceCreateCommand request, InvoiceMapper invoiceMapper,
+            InvoiceRepository invoiceRepository, TicketRepository ticketRepository) {
+        super(request, invoiceMapper, invoiceRepository);
+        this.ticketRepository = ticketRepository;
+    }
+
+    @Override
+    public InvoiceDto execute() {
+        List<Ticket> tickets = ticketRepository.findByIdIn(request.getTicketIds());
+
+        if (tickets.size() < request.getTicketIds().size()) {
+            throw new InvalidInputException("invoice.tickets.invalid");
+        }
+
+        Invoice invoice = invoiceMapper.toEntity(request);
+        invoice.setTickets(tickets);
+
+        Invoice savedInvoice = invoiceRepository.save(invoice);
+
+        return invoiceMapper.toDto(savedInvoice);
+    }
+}
