0ff02e073ba55f07a781c9a61ab3215ddd2345b8
An Tran Dinh Khanh <trandinhkhanhan12042003@gmail.com>
2025-10-02 16:54:00 +0700

    Add InvoiceUpdateHandler for updating invoices and associated tickets

---
+++ src/server/mms-api/src/main/java/com/mms/mms_api/business/handler/invoice/InvoiceUpdateHandler.java
@@ -0,0 +1,50 @@
+package com.mms.mms_api.business.handler.invoice;
+
+import com.mms.mms_api.business.command.invoice.InvoiceUpdateCommand;
+import com.mms.mms_api.data.InvoiceRepository;
+import com.mms.mms_api.data.TicketRepository;
+import com.mms.mms_api.dto.InvoiceDto;
+import com.mms.mms_api.exception.ResourceNotFoundException;
+import com.mms.mms_api.model.Invoice;
+import com.mms.mms_api.model.Ticket;
+import com.mms.mms_api.util.mapper.InvoiceMapper;
+
+import java.util.List;
+
+public class InvoiceUpdateHandler extends InvoiceBaseHandler<InvoiceUpdateCommand, InvoiceDto> {
+    private final TicketRepository ticketRepository;
+
+    public InvoiceUpdateHandler(InvoiceUpdateCommand request, InvoiceMapper invoiceMapper,
+            InvoiceRepository invoiceRepository, TicketRepository ticketRepository) {
+        super(request, invoiceMapper, invoiceRepository);
+        this.ticketRepository = ticketRepository;
+    }
+
+    @Override
+    public InvoiceDto execute() {
+        Invoice invoice = invoiceRepository.findById(request.getId())
+                .orElseThrow(() -> new ResourceNotFoundException("invoice.notFound"));
+
+        List<Ticket> tickets = ticketRepository.findByIdIn(request.getTicketIds());
+
+        if (tickets.size() < request.getTicketIds().size()) {
+            throw new IllegalArgumentException("invoice.tickets.invalid");
+        }
+
+        invoiceMapper.updateEntity(request, invoice);
+
+        for (Ticket oldTicket : invoice.getTickets()) {
+            oldTicket.setInvoice(null);
+        }
+
+        for (Ticket newTicket : tickets) {
+            newTicket.setInvoice(invoice);
+        }
+
+        invoice.setTickets(tickets);
+
+        Invoice updatedInvoice = invoiceRepository.save(invoice);
+
+        return invoiceMapper.toDto(updatedInvoice);
+    }
+}
