c91a13b9d989d4b6dce3452b43ca2ee0a9471c44
An Tran Dinh Khanh <trandinhkhanhan12042003@gmail.com>
2025-10-03 16:40:46 +0700

    Add ticketIds field to PromotionCreateCommand

--- src/server/mms-api/src/main/java/com/mms/mms_api/business/command/promotion/PromotionCreateCommand.java
+++ src/server/mms-api/src/main/java/com/mms/mms_api/business/command/promotion/PromotionCreateCommand.java
@@ -5,6 +5,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import java.time.LocalDate;
+import java.util.List;
+import java.util.UUID;
 
 @Data
 @EqualsAndHashCode(callSuper = true)
@@ -21,4 +23,6 @@ public class PromotionCreateCommand extends BaseCreateCommand {
     private String image;
 
     private int discount;
+
+    private List<UUID> ticketIds;
 }
