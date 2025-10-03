df2f07df384eff5ba10d7b3a2fa3fa38e3a9bcb9
An Tran Dinh Khanh <trandinhkhanhan12042003@gmail.com>
2025-10-03 16:57:33 +0700

    Add PromotionGetAllHandler and PromotionGetByIdHandler classes for handling promotion retrieval

---
+++ src/server/mms-api/src/main/java/com/mms/mms_api/business/handler/promotion/PromotionGetAllHandler.java
@@ -0,0 +1,21 @@
+package com.mms.mms_api.business.handler.promotion;
+
+import com.mms.mms_api.business.query.promotion.PromotionGetAllQuery;
+import com.mms.mms_api.dto.PromotionDto;
+import com.mms.mms_api.data.PromotionRepository;
+import com.mms.mms_api.util.mapper.PromotionMapper;
+import java.util.List;
+
+public class PromotionGetAllHandler extends PromotionBaseHandler<PromotionGetAllQuery, List<PromotionDto>> {
+
+    public PromotionGetAllHandler(PromotionGetAllQuery request, PromotionMapper promotionMapper, PromotionRepository promotionRepository) {
+        super(request, promotionMapper, promotionRepository);
+    }
+
+    @Override
+    public List<PromotionDto> execute() {
+        return promotionRepository.findAll().stream()
+                .map(promotionMapper::toDto)
+                .toList();
+    }
+}
---
+++ src/server/mms-api/src/main/java/com/mms/mms_api/business/handler/promotion/PromotionGetByIdHandler.java
@@ -0,0 +1,24 @@
+package com.mms.mms_api.business.handler.promotion;
+
+import com.mms.mms_api.business.query.promotion.PromotionGetByIdQuery;
+import com.mms.mms_api.dto.PromotionDto;
+import com.mms.mms_api.exception.ResourceNotFoundException;
+import com.mms.mms_api.model.Promotion;
+import com.mms.mms_api.data.PromotionRepository;
+import com.mms.mms_api.util.mapper.PromotionMapper;
+
+public class PromotionGetByIdHandler extends PromotionBaseHandler<PromotionGetByIdQuery, PromotionDto> {
+
+    public PromotionGetByIdHandler(PromotionGetByIdQuery request, PromotionMapper promotionMapper,
+            PromotionRepository promotionRepository) {
+        super(request, promotionMapper, promotionRepository);
+    }
+
+    @Override
+    public PromotionDto execute() {
+        Promotion promotion = promotionRepository.findById(request.getId())
+                .orElseThrow(() -> new ResourceNotFoundException("promotion.notFound"));
+
+        return promotionMapper.toDto(promotion);
+    }
+}
