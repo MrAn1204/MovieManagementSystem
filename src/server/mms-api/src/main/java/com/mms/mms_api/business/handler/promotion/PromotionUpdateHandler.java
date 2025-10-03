df85deda41769e3c1e0567ffc812f81a6e29c425
An Tran Dinh Khanh <trandinhkhanhan12042003@gmail.com>
2025-10-03 17:13:20 +0700

    Add PromotionUpdateHandler for updating promotions and associated tickets

---
+++ src/server/mms-api/src/main/java/com/mms/mms_api/business/handler/promotion/PromotionUpdateHandler.java
@@ -0,0 +1,49 @@
+package com.mms.mms_api.business.handler.promotion;
+
+import java.util.List;
+
+import com.mms.mms_api.business.command.promotion.PromotionUpdateCommand;
+import com.mms.mms_api.dto.PromotionDto;
+import com.mms.mms_api.exception.InvalidInputException;
+import com.mms.mms_api.exception.ResourceNotFoundException;
+import com.mms.mms_api.model.Promotion;
+import com.mms.mms_api.model.Ticket;
+import com.mms.mms_api.data.PromotionRepository;
+import com.mms.mms_api.data.TicketRepository;
+import com.mms.mms_api.util.mapper.PromotionMapper;
+
+public class PromotionUpdateHandler extends PromotionBaseHandler<PromotionUpdateCommand, PromotionDto> {
+    private final TicketRepository ticketRepository;
+
+    public PromotionUpdateHandler(PromotionUpdateCommand request, PromotionMapper promotionMapper,
+            PromotionRepository promotionRepository, TicketRepository ticketRepository) {
+        super(request, promotionMapper, promotionRepository);
+        this.ticketRepository = ticketRepository;
+    }
+
+    @Override
+    public PromotionDto execute() {
+        Promotion promotion = promotionRepository.findById(request.getId())
+                .orElseThrow(() -> new ResourceNotFoundException("promotion.notFound"));
+
+        List<Ticket> tickets = ticketRepository.findByIdIn(request.getTicketIds());
+
+        if (tickets.size() != request.getTicketIds().size()) {
+            throw new InvalidInputException("promotion.tickets.invalid");
+        }
+
+        promotionMapper.updateEntity(request, promotion);
+
+        for (Ticket oldTicket : promotion.getTickets()) {
+            oldTicket.setPromotion(null);
+        }
+
+        for (Ticket newTicket : tickets) {
+            newTicket.setPromotion(promotion);
+        }
+
+        Promotion updatedPromotion = promotionRepository.save(promotion);
+
+        return promotionMapper.toDto(updatedPromotion);
+    }
+}
