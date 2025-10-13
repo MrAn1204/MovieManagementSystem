1c4fda5aada41e1adcd77361a95b20247cc050a9
An Tran Dinh Khanh <trandinhkhanhan12042003@gmail.com>
2025-10-13 11:05:13 +0700

    Add @Transactional annotation to PromotionCreateHandler, PromotionDeleteHandler, and PromotionUpdateHandler

--- src/server/mms-api/src/main/java/com/mms/mms_api/business/handler/promotion/PromotionDeleteHandler.java
+++ src/server/mms-api/src/main/java/com/mms/mms_api/business/handler/promotion/PromotionDeleteHandler.java
@@ -1,7 +1,12 @@
 package com.mms.mms_api.business.handler.promotion;
 
+import org.springframework.transaction.annotation.Transactional;
+
 import com.mms.mms_api.business.command.promotion.PromotionDeleteCommand;
 import com.mms.mms_api.data.PromotionRepository;
+import com.mms.mms_api.exception.ResourceNotFoundException;
+import com.mms.mms_api.model.Promotion;
+import com.mms.mms_api.model.Ticket;
 
 public class PromotionDeleteHandler extends PromotionBaseHandler<PromotionDeleteCommand, Void> {
 
@@ -10,10 +15,17 @@ public class PromotionDeleteHandler extends PromotionBaseHandler<PromotionDelete
     }
 
     @Override
+    @Transactional
     public Void execute() {
-        var promotion = promotionRepository.findById(request.getId())
-                .orElseThrow(() -> new IllegalArgumentException("Promotion not found"));
+        Promotion promotion = promotionRepository.findById(request.getId())
+                .orElseThrow(() -> new ResourceNotFoundException("promotion.notFound"));
+        
+        for (Ticket ticket : promotion.getTickets()) {
+            ticket.setPromotion(null);
+        }
+        
         promotionRepository.delete(promotion);
+        
         return null;
     }
 }
