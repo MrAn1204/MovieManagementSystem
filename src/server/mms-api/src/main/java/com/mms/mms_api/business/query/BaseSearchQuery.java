dd17c8070d69f967351b7ed141d063fb31753062
An Tran Dinh Khanh <trandinhkhanhan12042003@gmail.com>
2025-10-14 09:26:28 +0700

    Refactor BaseSearchQuery to add validation

--- src/server/mms-api/src/main/java/com/mms/mms_api/business/query/BaseSearchQuery.java
+++ src/server/mms-api/src/main/java/com/mms/mms_api/business/query/BaseSearchQuery.java
@@ -2,25 +2,23 @@ package com.mms.mms_api.business.query;
 
 import java.io.Serializable;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public abstract class BaseSearchQuery implements BaseQuery, Serializable {
+    @Min(value = 1, message = "{search.pageNumber.min}")
     private int pageNumber = 1;
-    
-    private int pageSize = 10;
-    
-    private String keyword;
-    
-    private String sortBy = "id";
-    
-    private SortDirection sortDirection = SortDirection.ASC;
 
-    protected BaseSearchQuery(Integer pageNumber, Integer pageSize, String keyword, String sortBy, SortDirection sortDirection) {
-        this.pageNumber = pageNumber > 0 ? pageNumber : 1;
-        this.pageSize = pageSize > 0 ? pageSize : 10;
-        this.keyword = keyword;
-        this.sortBy = sortBy != null ? sortBy : "id";
-        this.sortDirection = sortDirection != null ? sortDirection : SortDirection.ASC;
-    }
+    @Min(value = 10, message = "{search.pageSize.min}")
+    private int pageSize = 10;
+
+    private String keyword;
+
+    @NotNull(message = "{search.sortBy.required}")
+    private String sortBy = "id";
+
+    @NotNull(message = "{search.sortDirection.required}")
+    private SortDirection sortDirection = SortDirection.ASC;
 }
