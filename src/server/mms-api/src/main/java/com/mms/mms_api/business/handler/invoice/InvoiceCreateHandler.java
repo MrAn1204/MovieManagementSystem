a4fe563a1c70caba403b678fc3fb63ac7567e072
An Tran Dinh Khanh <trandinhkhanhan12042003@gmail.com>
2025-10-17 10:34:17 +0700

    Update InvoiceCreateHandler and InvoiceUpdateHandler to handle updating User's score

--- src/server/mms-api/src/main/java/com/mms/mms_api/business/handler/invoice/InvoiceCreateHandler.java
+++ src/server/mms-api/src/main/java/com/mms/mms_api/business/handler/invoice/InvoiceCreateHandler.java
@@ -3,10 +3,13 @@ package com.mms.mms_api.business.handler.invoice;
 import com.mms.mms_api.business.command.invoice.InvoiceCreateCommand;
 import com.mms.mms_api.dto.InvoiceDto;
 import com.mms.mms_api.exception.InvalidInputException;
+import com.mms.mms_api.exception.ResourceNotFoundException;
 import com.mms.mms_api.data.InvoiceRepository;
 import com.mms.mms_api.data.TicketRepository;
+import com.mms.mms_api.data.UserRepository;
 import com.mms.mms_api.model.Invoice;
 import com.mms.mms_api.model.Ticket;
+import com.mms.mms_api.model.User;
 import com.mms.mms_api.util.mapper.InvoiceMapper;
 
 import java.util.List;
@@ -17,10 +20,13 @@ public class InvoiceCreateHandler extends InvoiceBaseHandler<InvoiceCreateComman
 
     private final TicketRepository ticketRepository;
 
+    private final UserRepository userRepository;
+
     public InvoiceCreateHandler(InvoiceCreateCommand request, InvoiceMapper invoiceMapper,
-            InvoiceRepository invoiceRepository, TicketRepository ticketRepository) {
+            InvoiceRepository invoiceRepository, TicketRepository ticketRepository, UserRepository userRepository) {
         super(request, invoiceMapper, invoiceRepository);
         this.ticketRepository = ticketRepository;
+        this.userRepository = userRepository;
     }
 
     @Override
@@ -39,6 +45,13 @@ public class InvoiceCreateHandler extends InvoiceBaseHandler<InvoiceCreateComman
         }
 
         invoice.setTickets(tickets);
+
+        User user = userRepository.findById(request.getUserId())
+                .orElseThrow(() -> new ResourceNotFoundException("user.notFound"));
+
+        user.setScore(request.getUseScore(), request.getAddScore());
+
+        invoice.setUser(user);
 
         Invoice savedInvoice = invoiceRepository.save(invoice);
 
--- src/server/mms-api/src/main/java/com/mms/mms_api/business/handler/invoice/InvoiceUpdateHandler.java
+++ src/server/mms-api/src/main/java/com/mms/mms_api/business/handler/invoice/InvoiceUpdateHandler.java
@@ -3,10 +3,13 @@ package com.mms.mms_api.business.handler.invoice;
 import com.mms.mms_api.business.command.invoice.InvoiceUpdateCommand;
 import com.mms.mms_api.data.InvoiceRepository;
 import com.mms.mms_api.data.TicketRepository;
+import com.mms.mms_api.data.UserRepository;
 import com.mms.mms_api.dto.InvoiceDto;
+import com.mms.mms_api.exception.InvalidInputException;
 import com.mms.mms_api.exception.ResourceNotFoundException;
 import com.mms.mms_api.model.Invoice;
 import com.mms.mms_api.model.Ticket;
+import com.mms.mms_api.model.User;
 import com.mms.mms_api.util.mapper.InvoiceMapper;
 
 import java.util.List;
@@ -16,10 +19,13 @@ import org.springframework.transaction.annotation.Transactional;
 public class InvoiceUpdateHandler extends InvoiceBaseHandler<InvoiceUpdateCommand, InvoiceDto> {
     private final TicketRepository ticketRepository;
 
+    private final UserRepository userRepository;
+
     public InvoiceUpdateHandler(InvoiceUpdateCommand request, InvoiceMapper invoiceMapper,
-            InvoiceRepository invoiceRepository, TicketRepository ticketRepository) {
+            InvoiceRepository invoiceRepository, TicketRepository ticketRepository, UserRepository userRepository) {
         super(request, invoiceMapper, invoiceRepository);
         this.ticketRepository = ticketRepository;
+        this.userRepository = userRepository;
     }
 
     @Override
@@ -31,7 +37,14 @@ public class InvoiceUpdateHandler extends InvoiceBaseHandler<InvoiceUpdateComman
         List<Ticket> tickets = ticketRepository.findByIdIn(request.getTicketIds());
 
         if (tickets.size() < request.getTicketIds().size()) {
-            throw new IllegalArgumentException("invoice.tickets.invalid");
+            throw new InvalidInputException("invoice.tickets.invalid");
+        }
+
+        User user = userRepository.findById(invoice.getUser().getId())
+                .orElseThrow(() -> new ResourceNotFoundException("user.notFound"));
+
+        if (user.getScore() < request.getUseScore()) {
+            throw new InvalidInputException("user.score.insufficient");
         }
 
         invoiceMapper.updateEntity(request, invoice);
@@ -45,6 +58,11 @@ public class InvoiceUpdateHandler extends InvoiceBaseHandler<InvoiceUpdateComman
         }
 
         invoice.setTickets(tickets);
+
+        int useScoreDifference = request.getUseScore() - invoice.getUseScore();
+        int addScoreDifference = request.getAddScore() - invoice.getAddScore();
+
+        user.setScore(useScoreDifference, addScoreDifference);
 
         Invoice updatedInvoice = invoiceRepository.save(invoice);
 
