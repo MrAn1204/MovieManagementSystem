8a0cac5474b6634582a96fe309f741ef2a7a02b8
An Tran Dinh Khanh <trandinhkhanhan12042003@gmail.com>
2025-10-21 16:23:50 +0700

    Add AuthBaseHandler class for handling authentication logic

---
+++ src/server/mms-api/src/main/java/com/mms/mms_api/business/handler/auth/AuthBaseHandler.java
@@ -0,0 +1,18 @@
+package com.mms.mms_api.business.handler.auth;
+
+import org.springframework.security.authentication.AuthenticationManager;
+
+import com.mms.mms_api.business.handler.BaseHandler;
+import com.mms.mms_api.util.JwtHelper;
+
+public abstract class AuthBaseHandler<I, O> extends BaseHandler<I, O> {
+    protected AuthenticationManager authenticationManager;
+
+    protected JwtHelper jwtHelper;
+
+    protected AuthBaseHandler(I request, AuthenticationManager authenticationManager, JwtHelper jwtHelper) {
+        super(request);
+        this.authenticationManager = authenticationManager;
+        this.jwtHelper = jwtHelper;
+    }
+}
