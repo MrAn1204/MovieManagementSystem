4d40e24447b7b0c1d1db01c911a2e9365aa436ad
An Tran Dinh Khanh <trandinhkhanhan12042003@gmail.com>
2025-10-21 16:25:27 +0700

    Add AuthenticationController for handling login requests

---
+++ src/server/mms-api/src/main/java/com/mms/mms_api/controller/AuthenticationController.java
@@ -0,0 +1,27 @@
+package com.mms.mms_api.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mms.mms_api.business.command.auth.LoginCommand;
+import com.mms.mms_api.business.service.AuthenticationService;
+
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+@RestController
+@RequestMapping("/auth")
+@AllArgsConstructor
+public class AuthenticationController {
+    private AuthenticationService authenticationService;
+    
+    @PostMapping("/login")
+    public String login(@Valid @RequestBody LoginCommand request) {
+        return authenticationService.handle(request);
+    }
+    
+}
