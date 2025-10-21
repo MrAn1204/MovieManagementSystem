2bb4867703ac7920d7a6c9e1cfaded2148f96a5a
An Tran Dinh Khanh <trandinhkhanhan12042003@gmail.com>
2025-10-21 16:20:57 +0700

    Add JwtAuthenticationFilter for JWT token authentication

---
+++ src/server/mms-api/src/main/java/com/mms/mms_api/security/JwtAuthenticationFilter.java
@@ -0,0 +1,59 @@
+package com.mms.mms_api.security;
+
+import java.io.IOException;
+import java.util.Collection;
+
+import org.springframework.lang.NonNull;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.mms.mms_api.business.service.UserInfoService;
+import com.mms.mms_api.util.JwtHelper;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.AllArgsConstructor;
+
+@Component
+@AllArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    private JwtHelper jwtHelper;
+
+    private UserInfoService authService;
+
+    @Override
+    protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response,
+            @NonNull FilterChain filterChain) throws ServletException, IOException {
+        String authHeader = request.getHeader("Authorization");
+
+        String jwt = null;
+        String username = null;
+
+        if (authHeader != null && authHeader.startsWith("Bearer ")) {
+            jwt = authHeader.substring(7);
+            username = jwtHelper.extractUsername(jwt);
+        }
+
+        if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            UserDetails userDetails = authService.loadUserByUsername(username);
+
+            if (jwtHelper.isTokenValid(jwt, userDetails.getUsername())) {
+                Collection<? extends GrantedAuthority> authority = userDetails.getAuthorities();
+
+                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
+                        userDetails, null, authority);
+
+                SecurityContextHolder.getContext().setAuthentication(authToken);
+            }
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+}
