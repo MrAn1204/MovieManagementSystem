0a8f939dd47b4ce25bbb8a15ef803dd21fc04187
An Tran Dinh Khanh <trandinhkhanhan12042003@gmail.com>
2025-11-01 21:07:50 +0700

    Add initial design for UI

---
+++ design/ui/script.js
@@ -0,0 +1,18 @@
+"use strict";
+
+function toggleSidebar() {
+    const sidebar = document.getElementsByTagName('aside')[0];
+    const overlay = document.getElementById('overlay');
+
+    sidebar.classList.toggle('left-0');
+    overlay.classList.toggle('hidden');
+}
+
+function toggleFilter() {
+    const filter = document.getElementsByClassName('filter')[0];
+    const filterButton = document.getElementsByClassName('filter-button')[0];
+    
+    filter.classList.toggle('hidden');
+    filterButton.classList.toggle('bg-munshellBlue');
+    filterButton.classList.toggle('text-white');
+}
