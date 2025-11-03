1af7810ada234fad6f13a9350d2692b0adb7c81b
An Tran Dinh Khanh <trandinhkhanhan12042003@gmail.com>
2025-11-03 10:37:14 +0700

    Refactor UI components to standardize text colors and improve hover effects; remove unused color definitions from Tailwind configuration

--- design/ui/tailwind.config.js
+++ design/ui/tailwind.config.js
@@ -6,8 +6,6 @@ tailwind.config = {
                 'french-gray': '#C5C3C6',
                 'space-black': '#46494C',
                 'munshell-blue': '#1985A1',
-                'black-eerie': '#1C1C1C',
-                'alabaster': '#ECEBE4',
                 'anti-flash-white': '#EEF0F2',
                 'ghost-white': '#FAFAFF',
                 'cerulean': '#006992',
