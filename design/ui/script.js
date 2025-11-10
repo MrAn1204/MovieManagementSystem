4b0f0112f29e0f48944555a6502488873644fdf4
An Tran Dinh Khanh <trandinhkhanhan12042003@gmail.com>
2025-11-10 11:29:23 +0700

    Add create/edit form and update script with image preview functionality

--- design/ui/script.js
+++ design/ui/script.js
@@ -1,5 +1,21 @@
 "use strict";
 
+document.addEventListener('DOMContentLoaded', function () {
+    const imageUpload = document.getElementById('imageUpload');
+    const imagePreview = document.getElementById('imagePreview');
+
+    imageUpload.addEventListener('change', function () {
+        const file = this.files[0];
+        if (file) {
+            const reader = new FileReader();
+            reader.onload = function (e) {
+                imagePreview.src = e.target.result;
+            };
+            reader.readAsDataURL(file);
+        }
+    });
+});
+
 function toggleSidebar() {
     const sidebar = document.getElementsByTagName('aside')[0];
     const overlay = document.getElementById('overlay');
@@ -12,7 +28,7 @@ function toggleFilter() {
     const filter = document.getElementsByClassName('filter')[0];
     const filterButton = document.getElementsByClassName('filter-button')[0];
     const filterBottom = document.getElementById('filter-bottom');
-    
+
     filter.classList.toggle('max-h-full');
     filter.classList.toggle('opacity-100');
     filterButton.classList.toggle('bg-munshell-blue');
