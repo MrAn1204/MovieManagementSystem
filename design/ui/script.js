b4a966745e7f4d71e69c9e6fd83e3b2009948c59
An Tran Dinh Khanh <trandinhkhanhan12042003@gmail.com>
2025-11-13 16:57:27 +0700

    Turn detail page into a modal

--- design/ui/script.js
+++ design/ui/script.js
@@ -47,3 +47,24 @@ function toggleOrder() {
     iconAsc.classList.toggle('hidden');
     iconDesc.classList.toggle('hidden');
 }
+
+function toggleDetailModal() {
+    const detailModal = document.getElementsByClassName('detail-modal')[0];
+    
+    detailModal.classList.toggle('hidden');
+    detailModal.classList.toggle('flex');
+}
+
+function toggleCreateModal() {
+    const createModal = document.getElementsByClassName('create-modal')[0];
+    
+    createModal.classList.toggle('hidden');
+    createModal.classList.toggle('flex');
+}
+
+function toggleEditModal() {
+    const editModal = document.getElementsByClassName('edit-modal')[0];
+    
+    editModal.classList.toggle('hidden');
+    editModal.classList.toggle('flex');
+}
