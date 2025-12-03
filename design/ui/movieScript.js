e8fdcf3652c30aa55e084b13aa8c7f67e9cc8011
An Tran Dinh Khanh <trandinhkhanhan12042003@gmail.com>
2025-12-03 17:06:40 +0700

    Add popup modal displayed when interact with table's buttons, rename some ids and classes in filter

---
+++ design/ui/movieScript.js
@@ -0,0 +1,51 @@
+"use strict";
+
+function toggleForm() {
+    resetForm();
+    toggleModal('create-edit-modal');
+}
+
+function submitForm(event) {
+    event.preventDefault();
+
+    for (const errorText of document.getElementsByClassName('error-text')) {
+        errorText.classList.add('hidden');
+    }
+
+    const form = document.getElementById('create-edit-form');
+    const formData = new FormData(form);
+
+    console.log(formData.get('name'));
+
+    let formValid = true;
+
+    if (formData.get('name') === null || formData.get('name').trim() === '') {
+        const nameError = document.getElementById('name-error');
+        nameError.classList.remove('hidden');
+        formValid = false;
+    }
+
+    if (formValid) {
+        form.submit();
+    }
+}
+
+function resetForm() {
+    onReset(['genre-display', 'studio-display', 'talent-display']);
+}
+
+function toggleCreate() {
+    const title = document.getElementById("create-edit-title");
+
+    title.textContent = "Create Movie";
+
+    toggleForm();
+}
+
+function toggleEdit() {
+    const title = document.getElementById("create-edit-title");
+
+    title.textContent = "Edit Movie";
+
+    toggleForm();
+}
