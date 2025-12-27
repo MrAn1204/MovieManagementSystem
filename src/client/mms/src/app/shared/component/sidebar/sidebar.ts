3b68f9f414064632b40e9102da8a8e96b253afc8
An Tran Dinh Khanh <trandinhkhanhan12042003@gmail.com>
2025-12-27 11:53:16 +0700

    Refactor sidebar component to use RouterLink for navigation

--- src/client/mms/src/app/shared/component/sidebar/sidebar.ts
+++ src/client/mms/src/app/shared/component/sidebar/sidebar.ts
@@ -1,12 +1,22 @@
 import { Component, input } from '@angular/core';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-sidebar',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './sidebar.html',
   styleUrl: './sidebar.css',
 })
 export class Sidebar {
+  readonly routes = [
+    { path: '/', label: 'Home', icon: 'fa-house' }, 
+    { path: '/movie', label: 'Movie', icon: 'fa-film' }, 
+    { path: '/schedule', label: 'Schedule', icon: 'fa-calendar' }, 
+    { path: '/room', label: 'Room', icon: 'fa-door-closed' }, 
+    { path: '/ticket', label: 'Ticket', icon: 'fa-ticket' },
+    { path: '/promotion', label: 'Promotion', icon: 'fa-percent' }, 
+    { path: '/user', label: 'User', icon: 'fa-users' }
+  ]
   visible = input<boolean>();
 
   constructor() {}
