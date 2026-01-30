0b17acd146aa6005fcc2d54a96dc52d09c11072c
An Tran Dinh Khanh <trandinhkhanhan12042003@gmail.com>
2026-01-30 10:54:26 +0700

    Fix main layout briefly displayed when not logged in

--- src/client/mms/src/app/shared/main-layout/main-layout.ts
+++ src/client/mms/src/app/shared/main-layout/main-layout.ts
@@ -3,6 +3,7 @@ import { Header } from "../component/header/header";
 import { Sidebar } from "../component/sidebar/sidebar";
 import { FlowbiteService } from '../../service/flowbite.service';
 import { RouterOutlet } from '@angular/router';
+import { AuthService } from '../../service/auth/auth.service';
 
 @Component({
   selector: 'app-main-layout',
@@ -13,13 +14,16 @@ import { RouterOutlet } from '@angular/router';
 export class MainLayout implements OnInit {
   sidebarVisible: boolean = false;
   filterVisible: boolean = false;
+  hasUser: boolean = false;
 
-  constructor(private readonly flowbiteService: FlowbiteService) { }
+  constructor(private readonly flowbiteService: FlowbiteService, private readonly authService: AuthService) { }
 
   ngOnInit(): void {
     this.flowbiteService.loadFlowbite((flowbite) => {
       flowbite.initFlowbite();
     });
+
+    this.hasUser = this.authService.isAuthenticated();
   }
 
   toggleSidebar(): void {
