7e6b02ae956c349dac10df77934fc57ccc03ea0e
An Tran Dinh Khanh <trandinhkhanhan12042003@gmail.com>
2026-03-02 15:29:17 +0700

    Update updateValue method signature to accept null

--- src/client/mms/src/app/shared/component/form/base-field/base-field.ts
+++ src/client/mms/src/app/shared/component/form/base-field/base-field.ts
@@ -10,30 +10,30 @@ import { ControlValueAccessor } from '@angular/forms';
 export class BaseField<T> implements ControlValueAccessor {
   labelText= input<string>('');
   idName = input.required<string>();
-  
+
   value: T | null = null;
   disabled = false;
-  
+
   protected onChange = (_: any) => {};
   protected onTouched = () => {};
 
   writeValue(value: T): void {
     this.value = value;
   }
-  
+
   registerOnChange(fn: any): void {
     this.onChange = fn;
   }
-  
+
   registerOnTouched(fn: any): void {
     this.onTouched = fn;
   }
-  
+
   setDisabledState?(isDisabled: boolean): void {
     this.disabled = isDisabled;
   }
-  
-  updateValue(value: T): void {
+
+  updateValue(value: T | null): void {
     this.value = value;
     this.onChange(value);
     this.onTouched();
