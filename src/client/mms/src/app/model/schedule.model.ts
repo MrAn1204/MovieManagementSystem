ab93ecf56d6a85cda1e2d9ee48868b5f3ad2a68c
An Tran Dinh Khanh <trandinhkhanhan12042003@gmail.com>
2026-03-03 16:09:43 +0700

    Add 'name' property to ScheduleModel interface

--- src/client/mms/src/app/model/schedule.model.ts
+++ src/client/mms/src/app/model/schedule.model.ts
@@ -2,6 +2,7 @@ import { IdNameModel } from '../shared/model/id-name.model';
 import { BaseEntityModel } from '../shared/model/base-entity.model';
 
 export interface ScheduleModel extends BaseEntityModel {
+  name: string;
   showTime: string;
   movie: IdNameModel;
   room: IdNameModel;
