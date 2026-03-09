04eb1e554824a098236d6267c847fdd9774aef60
An Tran Dinh Khanh <trandinhkhanhan12042003@gmail.com>
2026-03-09 12:15:05 +0700

    Refactor movie create-edit and detail components to be dialogs

--- src/client/mms/src/app/feature/movie/detail/movie-detail.ts
+++ src/client/mms/src/app/feature/movie/detail/movie-detail.ts
@@ -1,15 +1,33 @@
-import { Component, input } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { DetailImage } from "../../../shared/component/detail/detail-image/detail-image";
 import { DetailText } from "../../../shared/component/detail/detail-text/detail-text";
-import { MovieModel } from '../../../model/movie.model';
 import { FormatCellPipe } from '../../../shared/pipe/format-cell/format-cell-pipe';
+import { DIALOG_DATA } from '@angular/cdk/dialog';
+import { DialogDataModel } from '../../../shared/model/dialog/dialog-data.model';
+import { Detail } from "../../../shared/component/dialog/detail/detail";
+import { BaseDialog } from '../../../shared/component/dialog/base/base-dialog';
+import { MovieCreateEdit } from '../create-edit/movie-create-edit';
+import { PopupModal } from '../../../shared/component/dialog/popup-modal/popup-modal';
+import { MovieModel } from '../../../model/movie.model';
 
 @Component({
   selector: 'app-movie-detail',
-  imports: [DetailImage, DetailText, FormatCellPipe],
+  imports: [DetailImage, DetailText, FormatCellPipe, Detail],
   templateUrl: './movie-detail.html',
   styleUrl: './movie-detail.css',
 })
-export class MovieDetail {
-  model = input.required<MovieModel>();
+export class MovieDetail extends BaseDialog {
+  data: DialogDataModel<MovieModel> = inject(DIALOG_DATA);
+
+  get model(): MovieModel {
+    return this.data.model as MovieModel;
+  }
+
+  openUpdate(): void {
+    this.dialogService.triggerOpen(MovieCreateEdit)
+  }
+
+  openDelete(): void {
+    this.dialogService.triggerOpen(PopupModal);
+  }
 }
