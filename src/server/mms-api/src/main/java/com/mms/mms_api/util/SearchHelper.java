55991cab8d964fde85dfe9101eb69c9b0b5d134f
An Tran Dinh Khanh <trandinhkhanhan12042003@gmail.com>
2026-03-18 15:15:04 +0700

    Refactor search handlers to simplify pagination logic and utilize a new helper method for generating paginated results

--- src/server/mms-api/src/main/java/com/mms/mms_api/util/SearchHelper.java
+++ src/server/mms-api/src/main/java/com/mms/mms_api/util/SearchHelper.java
@@ -1,19 +1,30 @@
 package com.mms.mms_api.util;
 
+import java.util.List;
+import java.util.function.Function;
+
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.domain.Sort.Direction;
 import org.springframework.lang.NonNull;
+
+import com.mms.mms_api.common.PaginatedResult;
 
 public class SearchHelper {
     private SearchHelper() {
     }
 
     @NonNull
-    public static Pageable generatePageable(@NonNull Direction sortDirection, @NonNull String sortBy, int pageNumber, int pageSize) {
-        Sort sort = Sort.by(sortDirection, sortBy);
+    public static Pageable generatePageable(int pageNumber, int pageSize) {
+        return PageRequest.of(pageNumber - 1, pageSize);
+    }
 
-        return PageRequest.of(pageNumber - 1, pageSize, sort);
+    public static <T, R> PaginatedResult<R> generatePaginatedResult(Page<T> page, Function<T, R> mapFunction) {
+        List<R> mappedContent = page.getContent().stream()
+                .map(mapFunction)
+                .toList();
+
+        return new PaginatedResult<>(mappedContent, page.getTotalElements(), page.getTotalPages(),
+                page.getSize(), page.getNumber() + 1);
     }
 }
