f81627309c40bdfa7945d0e40f75976689dd51d5
An Tran Dinh Khanh <trandinhkhanhan12042003@gmail.com>
2026-03-21 11:26:05 +0700

    Add user filter component

---
+++ src/client/mms/src/app/feature/user/filter/user-filter.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit, signal } from '@angular/core';
+import { ControlContainer, FormGroupDirective, ReactiveFormsModule } from '@angular/forms';
+import { SelectField } from '../../../shared/component/form/select/select-field';
+import { FlowbiteService } from '../../../service/flowbite.service';
+import { RoleService } from '../../../service/role/role.service';
+import { FormOptionModel } from '../../../shared/model/form-option.model';
+
+@Component({
+  selector: 'app-user-filter',
+  imports: [ReactiveFormsModule, SelectField],
+  viewProviders: [{ provide: ControlContainer, useExisting: FormGroupDirective }],
+  templateUrl: './user-filter.html',
+  styleUrl: './user-filter.css',
+})
+export class UserFilter implements OnInit {
+  roles = signal<FormOptionModel[]>([]);
+
+  constructor(
+    private readonly roleService: RoleService,
+    private readonly flowbiteService: FlowbiteService
+  ) { }
+
+  ngOnInit(): void {
+    this.flowbiteService.loadFlowbite((flowbite) => {
+      flowbite.initFlowbite();
+    });
+
+    this.roleService.getAll().subscribe((roles) => {
+      const roleOptions: FormOptionModel[] = roles.map((role) => ({
+        label: role.name,
+        value: role.id,
+        selected: false,
+      }));
+
+      roleOptions.unshift({
+        label: 'Any Role',
+        value: '',
+        selected: true,
+      });
+
+      this.roles.set(roleOptions);
+    });
+  }
+}
