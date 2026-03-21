d18ed2ddec39c1a103784ea656e0c1db5c1a30b3
An Tran Dinh Khanh <trandinhkhanhan12042003@gmail.com>
2026-03-21 11:25:29 +0700

    Add user detail component

---
+++ src/client/mms/src/app/feature/user/detail/user-detail.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { DetailDialog } from '../../../shared/component/dialog/detail/detail-dialog';
+import { Detail } from '../../../shared/component/detail/detail-component/detail';
+import { DetailText } from '../../../shared/component/detail/detail-text/detail-text';
+import { FormatCellPipe } from '../../../shared/pipe/format-cell/format-cell-pipe';
+import { UserCreateEdit } from '../create-edit/user-create-edit';
+import { UserDetailModel } from '../../../model/user/user-detail.model';
+
+@Component({
+  selector: 'app-user-detail',
+  imports: [Detail, DetailText, FormatCellPipe],
+  templateUrl: './user-detail.html',
+  styleUrl: './user-detail.css',
+})
+export class UserDetail extends DetailDialog<UserDetailModel> {
+  protected override updateDialog = UserCreateEdit;
+}
