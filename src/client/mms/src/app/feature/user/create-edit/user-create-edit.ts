f0f01a4a1a8d7e231bf309a11b3c8648e11b63bc
An Tran Dinh Khanh <trandinhkhanhan12042003@gmail.com>
2026-03-21 11:25:46 +0700

    Add user create/edit component

---
+++ src/client/mms/src/app/feature/user/create-edit/user-create-edit.ts
@@ -0,0 +1,97 @@
+import { Component, OnInit, signal } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { CreateEditDialog } from '../../../shared/component/dialog/create-edit/create-edit-dialog';
+import { CreateEdit } from '../../../shared/component/create-edit/create-edit';
+import { InputField } from '../../../shared/component/form/input/input-field';
+import { SelectField } from '../../../shared/component/form/select/select-field';
+import { MultiselectField } from '../../../shared/component/form/multiselect/multiselect-field';
+import { ValidationError } from '../../../shared/component/form/error/validation-error';
+import { FormOptionModel } from '../../../shared/model/form-option.model';
+import { UserDetailModel } from '../../../model/user/user-detail.model';
+import { RoleService } from '../../../service/role/role.service';
+import { CustomValidators } from '../../../shared/util/custom-validators';
+import { ConstraintService } from '../../../service/constraint.service';
+import { Textarea } from "../../../shared/component/form/textarea/textarea-field";
+
+@Component({
+  selector: 'app-user-create-edit',
+  imports: [InputField, SelectField, MultiselectField, ValidationError, ReactiveFormsModule, CreateEdit, Textarea],
+  templateUrl: './user-create-edit.html',
+  styleUrl: './user-create-edit.css',
+})
+export class UserCreateEdit extends CreateEditDialog<UserDetailModel> implements OnInit {
+  readonly genders: FormOptionModel[] = [
+    { label: 'Male', value: 'MALE' },
+    { label: 'Female', value: 'FEMALE' },
+    { label: 'Other', value: 'OTHER' },
+  ];
+
+  roles = signal<FormOptionModel[]>([]);
+  changingPassword = signal(false);
+
+  private readonly constraints;
+
+  constructor(private readonly roleService: RoleService, private readonly constraintService: ConstraintService) {
+    super();
+
+    this.constraints = this.constraintService.get(
+      'USERNAME_MIN', 'USERNAME_MAX', 'FULLNAME_MIN', 'FULLNAME_MAX',
+      'PHONE_MIN', 'PHONE_MAX', 'ADDRESS_MIN', 'ADDRESS_MAX', 'PASSWORD_MIN'
+    );
+  }
+
+  get isEditMode(): boolean {
+    return !!this.data.model;
+  }
+
+  ngOnInit(): void {
+    this.applyPasswordMode(!this.isEditMode);
+    this.loadRoleOptions();
+  }
+
+  togglePasswordChange(status: boolean): void {
+    this.changingPassword.set(status);
+    this.applyPasswordMode(status);
+  }
+
+  private loadRoleOptions(): void {
+    const selectedRoleIds = this.data.model?.roles?.map((role) => role.id) ?? [];
+
+    this.roleService.getAll().subscribe((roles) => {
+      this.roles.set(
+        roles.map((role) => ({
+          label: role.name,
+          value: role.id,
+          selected: selectedRoleIds.includes(role.id),
+        }))
+      );
+    });
+  }
+
+  private applyPasswordMode(enabled: boolean): void {
+    const passwordControl = this.form.get('password');
+    const confirmPasswordControl = this.form.get('confirmPassword');
+
+    if (!passwordControl || !confirmPasswordControl) {
+      return;
+    }
+
+    if (enabled) {
+      passwordControl.addValidators([
+        CustomValidators.required('user.password.required'),
+        CustomValidators.passwordValid(this.constraints['PASSWORD_MIN'], 'user.password.invalid'),
+      ]);
+      confirmPasswordControl.addValidators([
+        CustomValidators.required('user.confirmPassword.required'),
+      ]);
+    } else {
+      passwordControl.clearValidators();
+      confirmPasswordControl.clearValidators();
+      passwordControl.setValue('');
+      confirmPasswordControl.setValue('');
+    }
+
+    passwordControl.updateValueAndValidity();
+    confirmPasswordControl.updateValueAndValidity();
+  }
+}
