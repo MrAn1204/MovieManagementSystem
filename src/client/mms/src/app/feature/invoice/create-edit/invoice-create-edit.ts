43f40f9f12818ab943f3b0bf9bea6e296e27407f
An Tran Dinh Khanh <trandinhkhanhan12042003@gmail.com>
2026-03-25 17:27:40 +0700

    Add invoice create/edit form template

--- src/client/mms/src/app/feature/invoice/create-edit/invoice-create-edit.ts
+++ src/client/mms/src/app/feature/invoice/create-edit/invoice-create-edit.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { CreateEditDialog } from '../../../shared/component/dialog/create-edit/create-edit-dialog';
 import { InvoiceModel } from '../../../model/invoice/invoice.model';
+import { CreateEdit } from "../../../shared/component/create-edit/create-edit";
+import { InputField } from "../../../shared/component/form/input/input-field";
+import { ValidationError } from "../../../shared/component/form/error/validation-error";
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-invoice-create-edit',
-  imports: [],
+  imports: [CreateEdit, InputField, ValidationError, ReactiveFormsModule],
   templateUrl: './invoice-create-edit.html',
   styleUrl: './invoice-create-edit.css',
 })
