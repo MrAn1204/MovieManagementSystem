c1f509b1721d6aeea5e7e0bd0814e8a4e7564c1d
An Tran Dinh Khanh <trandinhkhanhan12042003@gmail.com>
2026-04-25 11:50:24 +0700

    Refactor SelectField to use custom template similar to MultiselectField

--- src/client/mms/src/app/shared/component/form/select/select-field.ts
+++ src/client/mms/src/app/shared/component/form/select/select-field.ts
@@ -1,4 +1,4 @@
-import { Component, forwardRef, input } from '@angular/core';
+import { Component, effect, forwardRef, input } from '@angular/core';
 import { BaseField } from '../base-field/base-field';
 import { FormOptionModel } from '../../../model/form-option.model';
 import { NG_VALUE_ACCESSOR } from '@angular/forms';
@@ -18,4 +18,37 @@ import { NG_VALUE_ACCESSOR } from '@angular/forms';
 })
 export class SelectField extends BaseField<string> {
   options = input<FormOptionModel[]>([]);
+  keyword = '';
+  displayValue = '';
+
+  constructor() {
+    super();
+
+    effect(() => {
+      this.displayValue = this.options().find(option => option.value === this.value)?.label || '';
+    });
+  }
+
+  updateSelected(option: FormOptionModel): void {
+    if (this.value === option.value) {
+      this.updateValue('');
+      this.displayValue = '';
+    } else {
+      this.updateValue(option.value);
+      this.displayValue = option.label;
+    }
+  }
+
+  resetSelected(): void {
+    this.writeValue('');
+    this.displayValue = '';
+  }
+
+  updateKeyword(value: string): void {
+    this.keyword = value;
+  }
+
+  containKeyword(string: string): boolean {
+    return string.toLowerCase().includes(this.keyword.toLowerCase());
+  }
 }
