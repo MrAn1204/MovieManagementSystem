af516a37484cf86db5d1b131f9aa06d4cc161b4f
An Tran Dinh Khanh <trandinhkhanhan12042003@gmail.com>
2026-05-08 18:22:17 +0700

    Add SecurityAuditorAware and enable JPA auditing in SecurityConfiguration

---
+++ src/server/mms-api/src/main/java/com/mms/mms_api/security/SecurityAuditorAware.java
@@ -0,0 +1,16 @@
+package com.mms.mms_api.security;
+
+import java.util.Optional;
+
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+public class SecurityAuditorAware implements AuditorAware<String> {
+    @Override
+    public Optional<String> getCurrentAuditor() {
+        return Optional.ofNullable(SecurityContextHolder.getContext().getAuthentication())
+        .filter(Authentication::isAuthenticated)
+        .map(Authentication::getName);
+    }
+}
--- src/server/mms-api/src/main/java/com/mms/mms_api/security/SecurityConfiguration.java
+++ src/server/mms-api/src/main/java/com/mms/mms_api/security/SecurityConfiguration.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
@@ -25,6 +27,7 @@ import lombok.AllArgsConstructor;
 @Configuration
 @AllArgsConstructor
 @EnableMethodSecurity
+@EnableJpaAuditing
 public class SecurityConfiguration {
     private JwtAuthenticationFilter jwtAuthFilter;
 
@@ -96,5 +99,10 @@ public class SecurityConfiguration {
     @Bean
     PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    AuditorAware<String> auditorProvider() {
+        return new SecurityAuditorAware();
     }
 }
