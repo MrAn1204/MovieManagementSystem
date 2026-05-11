ab2aed5fe02c9d615e37cd3376301f28e3a58305
An Tran Dinh Khanh <trandinhkhanhan12042003@gmail.com>
2026-05-11 18:21:10 +0700

    Add audit information display to detail dialogs

--- src/client/mms/src/app/shared/component/detail/detail-component/detail.ts
+++ src/client/mms/src/app/shared/component/detail/detail-component/detail.ts
@@ -3,10 +3,11 @@ import { DialogButtons } from "../../dialog/dialog-buttons/dialog-buttons";
 import { AuthService } from '../../../../service/auth/auth.service';
 import { RoleConfigModel } from '../../../model/role-config.model';
 import { Button } from "../../button/button";
+import { FormatCellPipe } from '../../../pipe/format-cell/format-cell-pipe';
 
 @Component({
   selector: 'app-detail',
-  imports: [DialogButtons, Button],
+  imports: [DialogButtons, Button, FormatCellPipe],
   templateUrl: './detail.html',
   styleUrl: './detail.css',
 })
@@ -14,6 +15,13 @@ export class Detail {
   title = input<string>();
 
   roleConfig = input<RoleConfigModel | undefined>(undefined);
+
+  auditInfo = input<{
+    createdAt: string;
+    updatedAt: string;
+    createdBy: string;
+    updatedBy: string;
+  } | null>(null);
 
   constructor(private readonly authService: AuthService) {
   }
@@ -23,6 +31,10 @@ export class Detail {
   openDelete = output<void>();
 
   closeDialog = output<void>();
+
+  canSeeAuditInfo(): boolean {
+    return this.authService.includeRoles(['ADMIN']);
+  }
 
   canEdit(): boolean {
     return this.authService.includeRoles(this.roleConfig()?.edit ?? []);
--- src/client/mms/src/app/shared/component/dialog/detail/detail-dialog.ts
+++ src/client/mms/src/app/shared/component/dialog/detail/detail-dialog.ts
@@ -3,6 +3,7 @@ import { BaseDialog } from '../base/base-dialog';
 import { DIALOG_DATA } from '@angular/cdk/dialog';
 import { BaseEntityModel } from '../../../model/base-entity.model';
 import { DetailDialogDataModel } from '../../../model/dialog/detail-dialog-data.model';
+import { AuditableEntityModel } from '../../../model/auditable-entity.model';
 
 @Component({
   selector: 'app-detail-dialog',
@@ -10,13 +11,32 @@ import { DetailDialogDataModel } from '../../../model/dialog/detail-dialog-data.
   templateUrl: './detail-dialog.html',
   styleUrl: './detail-dialog.css',
 })
-export abstract class DetailDialog<T extends BaseEntityModel> extends BaseDialog {
+export abstract class DetailDialog<T extends (BaseEntityModel | AuditableEntityModel)> extends BaseDialog {
   data: DetailDialogDataModel<T> = inject(DIALOG_DATA);
 
   protected abstract readonly updateDialog: Type<BaseDialog>;
 
   get model(): T | undefined {
     return this.data.model;
+  }
+
+  get auditInfo() {
+    if (!this.isAuditable()) {
+      return null;
+    }
+
+    const model = this.model as AuditableEntityModel;
+
+    return {
+      createdAt: model.createdAt,
+      updatedAt: model.updatedAt,
+      createdBy: model.createdBy,
+      updatedBy: model.updatedBy,
+    };
+  }
+
+  private isAuditable() {
+    return this.model && 'createdAt' in this.model && 'updatedAt' in this.model && 'createdBy' in this.model && 'updatedBy' in this.model;
   }
 
   openUpdate(): void {
