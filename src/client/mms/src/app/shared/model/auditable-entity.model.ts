3643128e129c68c8656fd48b37a9a594c1c6013c
An Tran Dinh Khanh <trandinhkhanhan12042003@gmail.com>
2026-05-11 17:25:47 +0700

    Add createdBy and updatedBy fields to AuditableEntityModel

--- src/client/mms/src/app/shared/model/auditable-entity.model.ts
+++ src/client/mms/src/app/shared/model/auditable-entity.model.ts
@@ -3,4 +3,6 @@ import { BaseEntityModel } from "./base-entity.model";
 export interface AuditableEntityModel extends BaseEntityModel {
   createdAt: string;
   updatedAt: string;
+  createdBy: string;
+  updatedBy: string;
 }
