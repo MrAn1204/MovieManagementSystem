8e47c4aa7865f4779d9022fa8d46c6dc3d40e8b2
An Tran Dinh Khanh <trandinhkhanhan12042003@gmail.com>
2026-05-21 17:32:26 +0700

    Add @NoArgsConstructor annotation to PasswordResetToken class

--- src/server/mms-api/src/main/java/com/mms/mms_api/model/PasswordResetToken.java
+++ src/server/mms-api/src/main/java/com/mms/mms_api/model/PasswordResetToken.java
@@ -9,10 +9,12 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Data
 @EqualsAndHashCode(callSuper = true)
+@NoArgsConstructor
 public class PasswordResetToken extends BaseEntity {
     @Column(unique = true, nullable = false)
     private String token;
