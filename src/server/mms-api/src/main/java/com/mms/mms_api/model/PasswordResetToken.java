a5dfbcdac094b98d04b3b48407ad84ff108c97dc
An Tran Dinh Khanh <trandinhkhanhan12042003@gmail.com>
2026-05-21 16:47:57 +0700

    Add a constructor to initialize fields with generated token and set expiry

--- src/server/mms-api/src/main/java/com/mms/mms_api/model/PasswordResetToken.java
+++ src/server/mms-api/src/main/java/com/mms/mms_api/model/PasswordResetToken.java
@@ -3,6 +3,8 @@ package com.mms.mms_api.model;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+import com.mms.mms_api.util.RandomTokenHelper;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import lombok.Data;
@@ -26,5 +28,12 @@ public class PasswordResetToken extends BaseEntity {
 
     public boolean isExpired() {
         return LocalDateTime.now().isAfter(expiry);
+    }
+
+    public PasswordResetToken(UUID userId) {
+        this.token = RandomTokenHelper.generateToken();
+        this.userId = userId;
+        this.expiry = LocalDateTime.now().plusMinutes(15);
+        this.used = false;
     }
 }
