a362641697023d4aa07b17c0b974485a51d1add7
An Tran Dinh Khanh <trandinhkhanhan12042003@gmail.com>
2026-08-14 12:03:37 +0700

    Add visibility control methods to BaseDialogV2

--- src/client/mms/src/app/shared/component-v2/dialog/base-dialog/base-dialog.ts
+++ src/client/mms/src/app/shared/component-v2/dialog/base-dialog/base-dialog.ts
@@ -13,4 +13,16 @@ export abstract class BaseDialogV2 {
   onClose(result?: any): void {
     this.dialogRef.close(result);
   }
+
+  hideSelf(): void {
+    this.dialogRef.addPanelClass('opacity-0!');
+    this.dialogRef.addPanelClass('hidden!');
+    this.dialogRef.addPanelClass('pointer-events-none!');
+  }
+
+  showSelf(): void {
+    this.dialogRef.removePanelClass('opacity-0!');
+    this.dialogRef.removePanelClass('hidden!');
+    this.dialogRef.removePanelClass('pointer-events-none!');
+  }
 }
